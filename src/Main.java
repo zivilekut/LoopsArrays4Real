ac895a201de8b74733eb4f922a562153fb189cb0
zivilekut <zivilekut@gmail.com>
2024-03-15 16:27:37 +0200

    7-8 done

--- src/Main.java
+++ src/Main.java
@@ -31,12 +31,12 @@ public class Main {
         System.out.println("~~~~~~~~~~~~~~~~~~~~ 3 užduotis ~~~~~~~~~~~~~~~~~~~~");
 //3.Sukurkite masyvą iš dešimties augalų pavadinimų.
 
-        String [] plants = {"rožė", "tulpė", "orchidėja", "beržas", "ąžuolas", "gluosnis", "alyva", "raskila", "klevas", "begonija"};
+        String[] plants = {"rožė", "tulpė", "orchidėja", "beržas", "ąžuolas", "gluosnis", "alyva", "raskila", "klevas", "begonija"};
 
         System.out.println("~~~~~~~~~~~~~~~~~~~~ 4 užduotis ~~~~~~~~~~~~~~~~~~~~");
 //4.Atspausdinkite kiekvieną 3čio uždavinio augalą atskiroje eilutėje.
 
-        for (int i = 0; i <10 ; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println(plants[i]);
         }
 
@@ -51,24 +51,42 @@ public class Main {
 //6.Atspausdinkite kas antrą skaičių nuo 10 iki 50 (porinius);
 
         for (int i = 10; i < 51; i++) {
-           if (i % 2 != 0){
-               continue;
-           }
+            if (i % 2 != 0) {
+                continue;
+            }
             System.out.println(i);
         }
-//7.Atspausdinkite kas antrą skaičių nuo 10 iki 50. (porinius) Jei skaičius dalinasi iš 10 be liekanos jo nespausdinkite. ( naudokite continue.) (atspausdinti visus porinus skaičius, išskyrus tuos kurie dalinasi iš 10 be liekanos)
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~ 7 užduotis ~~~~~~~~~~~~~~~~~~~~");
+//7.Atspausdinkite kas antrą skaičių nuo 10 iki 50. (porinius) Jei skaičius dalinasi iš 10 be liekanos jo nespausdinkite.
+// ( naudokite continue.) (atspausdinti visus porinus skaičius, išskyrus tuos kurie dalinasi iš 10 be liekanos)
+        for (int i = 10; i < 51; i++) {
+            if (i % 2 != 0) {
+                continue;
+            }
+            if (i % 10 != 0) {
+                System.out.println(i);
+            }
+        }
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~ 8 užduotis ~~~~~~~~~~~~~~~~~~~~");
 //8.Sukurkite ciklą kuris suktųsi nuo 0 iki 20. Suskaičiuokite, kiek kartų kintamasis i turėjo porinę reikšmę;
-//9.Suskaičiuokite kiek 3čio uždavinio masyve yra žodžių trumpesnių nei 5 simboliai, ir kiek ilgesnių nei 7 simboliai. (du skaičiavimai)
+        int counter = 0;
+        for (int i = 0; i < 21; i++) {
+            if (i % 2 != 0) {
+                continue;
+            }
+            counter++;
+            System.out.println(i);
+
+        } System.out.println("count:" + counter);
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~ 9 užduotis ~~~~~~~~~~~~~~~~~~~~");
+//9.Suskaičiuokite kiek 3čio uždavinio masyve yra žodžių trumpesnių nei 5 simboliai, ir kiek ilgesnių nei 7 simboliai.
+// (du skaičiavimai)
+
 //10.Suskaičiuokite kiek 3čio uždavinio masyve yra žodžių ilgesnių nei 5 simboliai bet trumpesnių  nei 10 simboliai. (tarp 5 ir 10 simbolių ilgio)
 
 
-
-
-
-
-
-
-
-        
     }
 }
