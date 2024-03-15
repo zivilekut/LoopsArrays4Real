b5719ded583d2ffe6c58ed4266227116cfea2101
zivilekut <zivilekut@gmail.com>
2024-03-15 15:50:15 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+public class Main {
+    public static void main(String[] args) {
+        /*ciklai:
+        for
+        foreach
+        while - naudojam, kai nezinom, kiek kartu prasisuks
+        do while
+
+        (js kalboje)
+        forin
+        forout
+
+        continue - toliau vykdo koda nuo pradines salygos
+        break - sustoja vykdyti
+         */
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~ 1 užduotis ~~~~~~~~~~~~~~~~~~~~");
+//1. Sukurkite ciklą kuris atspausdintų 10 kartų žodį “labas”.
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Labas" + i);
+        }
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~ 2 užduotis ~~~~~~~~~~~~~~~~~~~~");
+//2.Sukurkite ciklą kuris atspausdintų skaičius nuo 0 iki 9.
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~ 3 užduotis ~~~~~~~~~~~~~~~~~~~~");
+//3.Sukurkite masyvą iš dešimties augalų pavadinimų.
+
+        String [] plants = {"rožė", "tulpė", "orchidėja", "beržas", "ąžuolas", "gluosnis", "alyva", "raskila", "klevas", "begonija"};
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~ 4 užduotis ~~~~~~~~~~~~~~~~~~~~");
+//4.Atspausdinkite kiekvieną 3čio uždavinio augalą atskiroje eilutėje.
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(plants[i]);
+        }
+
+        System.out.println("~~~~~~~~~~~~~~~~~~~~ 5 užduotis ~~~~~~~~~~~~~~~~~~~~");
+//5.Atspausdinkite 3čio uždavinio kiekvieną augalą pradedant nuo paskutinio, ir baigiant pirmuoju.
+// (atvirkščias ciklas).
+
+
+//6.Atspausdinkite kas antrą skaičių nuo 10 iki 50 (porinius);
+//7.Atspausdinkite kas antrą skaičių nuo 10 iki 50. (porinius) Jei skaičius dalinasi iš 10 be liekanos jo nespausdinkite. ( naudokite continue.) (atspausdinti visus porinus skaičius, išskyrus tuos kurie dalinasi iš 10 be liekanos)
+//8.Sukurkite ciklą kuris suktųsi nuo 0 iki 20. Suskaičiuokite, kiek kartų kintamasis i turėjo porinę reikšmę;
+//9.Suskaičiuokite kiek 3čio uždavinio masyve yra žodžių trumpesnių nei 5 simboliai, ir kiek ilgesnių nei 7 simboliai. (du skaičiavimai)
+//10.Suskaičiuokite kiek 3čio uždavinio masyve yra žodžių ilgesnių nei 5 simboliai bet trumpesnių  nei 10 simboliai. (tarp 5 ir 10 simbolių ilgio)
+
+
+
+
+
+
+
+
+
+        
+    }
+}
